259f269a20844431c7e3c1e3ddb4621e6c577bba
Dave Eddy <dave@daveeddy.com>
2012-08-13 00:54:53 -0700

    webamp.js

--- site/static/js/webamp.js
+++ site/static/js/webamp.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  var cache = {};
+  var cache = {
+    'artists': null,
+    'albums': null,
+    'songs': null,
+    'songs_by_album': null,
+    'albums_by_artist': null
+  };
+  Object.keys(cache).forEach(function(key) {
+    $.getJSON('/api/'+key, function(data) {
+      console.log('Got ' + key);
+      cache[key] = data;
+    });
+  });
 });
